1207af83abc5b40e22a83feffaa3569750e510e4
Drag0nus <staffman2@gmail.com>
2015-04-23 19:28:37 +0300

    commit

--- test.js
+++ test.js
@@ -3,8 +3,32 @@
  */
 
 
+var express = require('express');
+var bodyparser = require('body-parser');
 
-for (var i = 0; i <= Number.MAX_VALUE; i++) {
-    console.log('Ow maaan!');
-}
+var app = express();
 
+app.use(bodyparser.json());
+
+app.get('/info', function(reg, res, next) {
+    res.status(200).send({success: 'Hello, World!'});
+});
+
+//app.get('/info2', function(reg, res, next) {
+//    res.redirect(301, 'http://pornhub.com');
+//});
+
+app.post('/info', function(reg, res, next) {
+    var body = reg.body;
+    if( !body.key ) {
+        var err = new Error('Not key');
+
+        return res.status(500).send({error: err.message + ' ' + err.stack});
+    }
+    res.status(200).send(body);
+});
+
+
+app.listen(3030, function() {
+    console.log('Server start on port = 3030')
+});
