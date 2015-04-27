e5f8fc85fee6772ad890c00c61bbd8ac7a0e2dfe
Drag0nus <staffman2@gmail.com>
2015-04-27 19:42:00 +0300

    lesson 27.05.15

--- test.js
+++ test.js
@@ -2,33 +2,73 @@
  * Created by dragonus on 23.04.15.
  */
 
-
 var express = require('express');
 var bodyparser = require('body-parser');
 
 var app = express();
 
-app.use(bodyparser.json());
-
-app.get('/info', function(reg, res, next) {
-    res.status(200).send({success: 'Hello, World!'});
-});
-
-//app.get('/info2', function(reg, res, next) {
-//    res.redirect(301, 'http://pornhub.com');
+//app.use(bodyparser.json());
+//
+//app.get('/info', function(reg, res, next) {
+//    res.status(200).send({success: 'Hello, World!'});
+//});
+//
+////app.get('/info2', function(reg, res, next) {
+////    res.redirect(301, 'http://pornhub.com');
+////});
+//
+//app.post('/info', function(reg, res, next) {
+//    var body = reg.body;
+//    if( !body.key ) {
+//        var err = new Error('Not key');
+//
+//        return res.status(500).send({error: err.message + ' ' + err.stack});
+//    }
+//    res.status(200).send(body);
 //});
 
-app.post('/info', function(reg, res, next) {
-    var body = reg.body;
-    if( !body.key ) {
-        var err = new Error('Not key');
+//
+//function checkIt(req, res, next) {
+//    console.log('we suck');
+//    var err = new Error('Bad request');
+//    err.status = 403;
+//    next(err);
+//};
 
-        return res.status(500).send({error: err.message + ' ' + err.stack});
+function errHandler (err, req, res, next) {
+    var status = err.status || 500;
+    var message = err.message + '\n\r' + err.stack;
+    //TODO check env production || development
+
+    res.status(status).send(message);
+    console.log(app.get('env'));
+};
+
+app.use(function(req, res, next) {
+    var ip = req.ip;
+    console.log(ip);
+    if (/::|127.0/.test(ip)) {
+        return next();
     }
-    res.status(200).send(body);
+    var err = new Error('Bad request');
+    err.status = 403;
+    next(err);
 });
+
+app.all('*', function(req, res, next) {
+    console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxx');
+    next();
+});
+
+app.get('/' /*, checkIt*/, function(req, res, next) {
+    console.log(req.headers['user-agent']);
+    res.status(200).send('You suck!');
+});
+
+app.use(errHandler);
 
 
 app.listen(3030, function() {
-    console.log('Server start on port = 3030')
+    console.log('Server start on pron = 3030')
 });
+
